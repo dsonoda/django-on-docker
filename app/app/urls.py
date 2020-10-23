6d1e560bfae9a6a35eb15d44adee14e2774f1012
園田大輔 <>
2020-10-23 09:36:23 +0900

    modify base settings

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
