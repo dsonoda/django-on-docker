d90a719e08e58378f274d55932c504ac6c1fcca7
園田大輔 <>
2020-10-23 10:49:12 +0900

    add app "upload"

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from uploads.views import image_upload
 
 
 urlpatterns = [
+    path("uploads/", image_upload, name="uploads"),
     path('admin/', admin.site.urls),
 ]
 
